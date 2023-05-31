abb2b467fc6d4563871ccbaad7aa2dbef14a7ef4
Manuel <nacermanuel@gmail.com>
2023-05-30 23:28:13 -0500

    first commit backend

---
+++ backend/src/main/java/com/devsarrolloit/DevSarrolloIT/DevSarrolloItApplication.java
@@ -0,0 +1,13 @@
+package com.devsarrolloit.DevSarrolloIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSarrolloItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSarrolloItApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsarrolloit/DevSarrolloIT/DevSarrolloItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsarrolloit.DevSarrolloIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSarrolloItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
